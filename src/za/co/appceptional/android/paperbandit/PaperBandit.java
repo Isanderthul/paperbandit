f7d7e7f2c9f8918edf82aa5094099bd0526fa284
warren <warren@jadsys.co.za>
2013-07-20 14:53:59 +0200

    More refactoring

---
+++ src/za/co/appceptional/android/paperbandit/PaperBandit.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.appceptional.android.paperbandit;
+
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.service.wallpaper.WallpaperService;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+
+/*
+ * This animated wallpaper draws a rotating wireframe shape. It is similar to
+ * example #1, but has a choice of 2 shapes, which are user selectable and
+ * defined in resources instead of in code.
+ */
+
+public class PaperBandit extends WallpaperService {
+
+	public static final String SHARED_PREFS_NAME = "paperbanditsettings";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public Engine onCreateEngine() {
+		return new PaperBanditEngine();
+	}
+
+	class PaperBanditEngine extends Engine implements
+			SharedPreferences.OnSharedPreferenceChangeListener {
+
+		private final Handler mHandler = new Handler();
+
+		private final Paint mPaint = new Paint();
+		private final Bitmap mBackground;
+		private float mOffset;
+		private float mTouchX = -1;
+		private float mTouchY = -1;
+		private long mStartTime;
+		private float mCenterX;
+		private float mCenterY;
+
+		private final Runnable mDrawCube = new Runnable() {
+			public void run() {
+				drawFrame();
+			}
+		};
+		private boolean mVisible;
+		private SharedPreferences mPrefs;
+
+		PaperBanditEngine() {
+            // Create a Paint to draw the lines for our cube
+            final Paint paint = mPaint;
+            mBackground = getImage(R.drawable.wrinkled_paper);
+            paint.setColor(0xffffffff);
+            paint.setAntiAlias(true);
+            paint.setStrokeWidth(2);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            paint.setStyle(Paint.Style.STROKE);
+
+            mStartTime = SystemClock.elapsedRealtime();
+
+            mPrefs = PaperBandit.this.getSharedPreferences(SHARED_PREFS_NAME, 0);
+            mPrefs.registerOnSharedPreferenceChangeListener(this);
+            onSharedPreferenceChanged(mPrefs, null);
+        }
+
+		/**
+		 * Load an image
+		 * 
+		 * @param id
+		 * @return Bitmap
+		 */
+		protected Bitmap getImage(int id) {
+			return BitmapFactory.decodeResource(getApplicationContext()
+					.getResources(), id);
+		}
+
+		public void onSharedPreferenceChanged(SharedPreferences prefs,
+				String key) {
+		}
+
+		@Override
+		public void onCreate(SurfaceHolder surfaceHolder) {
+			super.onCreate(surfaceHolder);
+			setTouchEventsEnabled(true);
+		}
+
+		@Override
+		public void onDestroy() {
+			super.onDestroy();
+			mHandler.removeCallbacks(mDrawCube);
+		}
+
+		@Override
+		public void onVisibilityChanged(boolean visible) {
+			mVisible = visible;
+			if (visible) {
+				drawFrame();
+			} else {
+				mHandler.removeCallbacks(mDrawCube);
+			}
+		}
+
+		@Override
+		public void onSurfaceChanged(SurfaceHolder holder, int format,
+				int width, int height) {
+			super.onSurfaceChanged(holder, format, width, height);
+			// store the center of the surface, so we can draw the cube in the
+			// right spot
+			mCenterX = width / 2.0f;
+			mCenterY = height / 2.0f;
+			drawFrame();
+		}
+
+		@Override
+		public void onSurfaceCreated(SurfaceHolder holder) {
+			super.onSurfaceCreated(holder);
+		}
+
+		@Override
+		public void onSurfaceDestroyed(SurfaceHolder holder) {
+			super.onSurfaceDestroyed(holder);
+			mVisible = false;
+			mHandler.removeCallbacks(mDrawCube);
+		}
+
+		@Override
+		public void onOffsetsChanged(float xOffset, float yOffset, float xStep,
+				float yStep, int xPixels, int yPixels) {
+			mOffset = xOffset;
+			drawFrame();
+		}
+
+		/*
+		 * Store the position of the touch event so we can use it for drawing
+		 * later
+		 */
+		@Override
+		public void onTouchEvent(MotionEvent event) {
+			if (event.getAction() == MotionEvent.ACTION_MOVE) {
+				mTouchX = event.getX();
+				mTouchY = event.getY();
+			} else {
+				mTouchX = -1;
+				mTouchY = -1;
+			}
+			super.onTouchEvent(event);
+		}
+
+		/*
+		 * Draw one frame of the animation. This method gets called repeatedly
+		 * by posting a delayed Runnable. You can do any drawing you want in
+		 * here. This example draws a wireframe cube.
+		 */
+		void drawFrame() {
+			final SurfaceHolder holder = getSurfaceHolder();
+			final Rect frame = holder.getSurfaceFrame();
+			final int width = frame.width();
+			final int height = frame.height();
+
+			Canvas c = null;
+			try {
+				c = holder.lockCanvas();
+				if (c != null) {
+					// draw something
+					drawTouchPoint(c);
+				}
+			} finally {
+				if (c != null)
+					holder.unlockCanvasAndPost(c);
+			}
+
+			mHandler.removeCallbacks(mDrawCube);
+			if (mVisible) {
+				mHandler.postDelayed(mDrawCube, 1000 / 25);
+			}
+		}
+
+		void drawTouchPoint(Canvas c) {
+			if (mTouchX >= 0 && mTouchY >= 0) {
+				c.drawCircle(mTouchX, mTouchY, 80, mPaint);
+			}
+		}
+	}
+}
